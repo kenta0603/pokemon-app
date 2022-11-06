3a94add1703e3ace82766d075aec073bb51c8c05
kenta0603 <noda.5552.k@gmail.com>
2022-11-06 20:30:37 +0900

    1106

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav>Navbar</nav>
+    <header>
+      <div className='logo'>
+        <h3>ポケモンAPIで遊ぶ</h3>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">ポケモン図鑑</Link>
+          </li>
+          <li>
+            <Link to="/quiz">ポケモンクイズ</Link>
+          </li>
+        </ul>
+      </nav>   
+    </header>
   )
 }
 
---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+
+const Quiz = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Quiz</div>
+    </>
+  )
+}
+
+export default Quiz
